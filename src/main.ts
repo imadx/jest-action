a2e7b5f2c212e05070acfd4bceafa157520a35d7
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-06-09 17:38:55 +0530

    use composite pipeline

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,6 @@ async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
     core.setOutput('time', new Date().toTimeString())
 
     core.info('Install dependencies...')
