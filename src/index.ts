0f7cc3543011f692389277ddab5332ac064a773e
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-06-10 21:19:23 +0530

    remove duplicate code

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,3 @@
-import { error, getBooleanInput, getInput, setFailed } from "@actions/core";
-import { info } from "console";
-import { mergeCoverage } from "./steps/merge-coverage";
-import { runTests } from "./steps/run-tests";
-
-async function run() {
-  const command = getInput("command");
-
-  info(`Running command: ${command}...`);
-
-  switch (command) {
-    case "run-tests":
-      await runTests({
-        coverage: getBooleanInput("coverage"),
-        shard: getInput("shard"),
-        skipArtifactUpload: getBooleanInput("skip-artifact-upload"),
-      });
-      break;
-    case "merge-coverage":
-      await mergeCoverage({
-        token: getInput("github-token"),
-        skipArtifactUpload: getBooleanInput("skip-artifact-upload"),
-        shardCount: +getInput("shard-count"),
-      });
-      break;
-    default:
-      error(`Invalid command: ${command}`);
-      setFailed(`Invalid command: ${command}`);
-  }
-
-  info(`Running command: ${command}... DONE`);
-}
+import { run } from "./run";
 
 run();
