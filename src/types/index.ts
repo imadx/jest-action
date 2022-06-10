bfe023c9cdd26f963ee521fb1eabb44214dbf497
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-06-10 16:35:08 +0530

    refactor types and table formatting

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,16 +2,21 @@ export interface ExecError {
   stdout: Buffer | string;
 }
 
-export interface SummaryTotal {
-  lines: SummaryTotalStats;
-  statements: SummaryTotalStats;
-  functions: SummaryTotalStats;
-  branches: SummaryTotalStats;
+export interface CoverageMetric {
+  lines: CoverageMetricDetails;
+  statements: CoverageMetricDetails;
+  functions: CoverageMetricDetails;
+  branches: CoverageMetricDetails;
 }
 
-export interface SummaryTotalStats {
+export interface CoverageMetricDetails {
   total: number;
   covered: number;
   skipped: number;
   pct: number;
+}
+
+export interface CoverageSummary {
+  total: CoverageMetric;
+  [filename: string]: CoverageMetric;
 }
